b0da3b44075cc9b3ccd328ca05919129f728af46
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-03-18 20:32:33 -0300

    feat: create design system using styled components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, useFonts } from '@expo-google-fonts/inter';
+import { ThemeProvider } from 'styled-components';
+import theme from '@theme/index';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+  });
+
+  if(!fontsLoaded) {
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size={32} color='#1A1A1A' />
+    </View>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar barStyle="dark-content" translucent backgroundColor='transparent' />
+      </View>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,46 @@
+export default {
+  colors: {
+    gray: {
+      100: '#F2F2F2',
+      200: '#E4E4E4',
+      300: '#C8C8C8',
+      400: '#909090',
+      500: '#333333',
+      600: '#292929',
+      700: '#1A1A1A'
+    },
+    purple: {
+      300: '#7134BF',
+      400: '#5610B2',
+      500: '#4B1F84'
+    },
+    orange: {
+      300: '#B85300',
+      400: '#E06500',
+      500: '#FF780A'
+    },
+    yellow: {
+      500: '#E3BE17'
+    },
+    red: {
+      500: '#FF0000'
+    },
+    green: {
+      500: '#00FF29'
+    }
+  },
+  typography: {
+    commonText: {
+      fontFamily: 'Inter_400Regular',
+      fontSize: 16
+    },
+    pageTitle: {
+      fontFamily: 'Inter_700Bold',
+      fontSize: 28
+    },
+    sectionTitle: {
+      fontFamily: 'Inter_600Bold',
+      fontSize: 20
+    }
+  }
+};
